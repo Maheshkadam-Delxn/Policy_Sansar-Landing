31f527136da164371e0934ac65cb8f2b4cd0fa2b
Maheshkadam-Delxn <maheshkadam.delxn@gmail.com>
2025-01-10 19:02:15 +0530

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,6 @@ import Image from "next/image";
 import Our_Partners from "./components/Our_Partners";
 import Insurances from "./components/Insurances";
 import Why_Choose from "./components/Why_Choose";
-import Experts from "./components/Experts";
-import Contact from "./components/Contact";
 import Homee from "./components/Homee";
 import Unique from "./components/Unique";
 export default function Home() {
@@ -14,7 +12,5 @@ export default function Home() {
     <Insurances/>
     <Why_Choose/>
     <Unique/>
-    {/* <Experts/> */}
-    {/* <Contact/> */}
     </div>
 );}
